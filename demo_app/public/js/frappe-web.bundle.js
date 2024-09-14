7412cce464ffe7d1bc0223bc3fb842e0177bec1c
root <root@LAPTOP-VOFV5O04.localdomain>
2024-09-14 15:01:35 +0530

    fixed share log in load.py file

---
+++ demo_app/public/js/frappe-web.bundle.js
@@ -0,0 +1,26 @@
+import "../../../../frappe/frappe/public/js/libs.bundle.js";
+import "../../../../frappe/frappe/public/js/frappe/class.js";
+import "../../../../frappe/frappe/public/js/frappe/polyfill.js";
+import "../../../../frappe/frappe/public/js/frappe/provide.js";
+import "../../../../frappe/frappe/public/js/frappe/translate.js";
+import "../../../../frappe/frappe/public/js/frappe/form/formatters.js";
+import "../../../../frappe/frappe/public/js/frappe/format.js";
+import "../../../../frappe/frappe/public/js/frappe/utils/number_format.js";
+import "../../../../frappe/frappe/public/js/frappe/utils/utils.js";
+import "../../../../frappe/frappe/public/js/frappe/utils/common.js";
+import "../../../../frappe/frappe/public/js/frappe/ui/messages.js";
+import "../../../../frappe/frappe/public/js/frappe/utils/pretty_date.js";
+import "../../../../frappe/frappe/public/js/frappe/utils/datetime.js";
+import "../../../../frappe/frappe/public/js/frappe/microtemplate.js";
+import "../../../../frappe/frappe/public/js/frappe/query_string.js";
+
+import "../../../../frappe/frappe/public/js/frappe/upload.js";
+
+import "../../../../frappe/frappe/public/js/frappe/model/meta.js";
+import "./frappe/model/model.js";
+import "../../../../frappe/frappe/public/js/frappe/model/perm.js";
+
+import "../../../../frappe/frappe/public/js/bootstrap-4-web.bundle";
+
+import "../../../../frappe/frappe/website/js/website.js";
+import "../../../../frappe/frappe/public/js/frappe/socketio_client.js";
